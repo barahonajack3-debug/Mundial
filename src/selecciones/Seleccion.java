4c4b4f30196e82e3606e377ceb039a41fdaa9f16
EMMAXZZ <emmnuelpinda@gmail.com>
2026-06-18 16:56:02 -0600

    clase seleccion
    aun esta incompleta

--- src/selecciones/Seleccion.java
+++ src/selecciones/Seleccion.java
@@ -4,10 +4,51 @@
  */
 package selecciones;
 
-/**
- *
- * @author EMMAXZZ
- */
-public class Seleccion {
+import jugadores.Jugador;
+
+
+
+public class Seleccion extends Jugador {
+
+    private String nombre;
+    private String grupo;
+    private String entrenador;
+    private String[] jugadores;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getEntrenador() {
+        return entrenador;
+    }
+
+    public void setEntrenador(String entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public String[] getJugadores() {
+        return jugadores;
+    }
+    
+     public Seleccion(String nombre, String grupo) {
+        this.nombre = nombre;
+        this.grupo = grupo;
+        this.jugadores = new String[23];
+        
+    }
+     public void agregarJugador(jugador jugador) {
+    
+}
+
+public void eliminarJugador(jugador jugador) {
     
 }
