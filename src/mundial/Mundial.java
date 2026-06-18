ffca16706fc02941eb0c5426fa542319714b7f6b
Daniel <danielariassolano@gmail.com>
2026-06-18 15:33:06 -0600

    se modifico la clase mundial
    se le agrego sus atributos con sus metodos get, constructor y toString

--- src/mundial/Mundial.java
+++ src/mundial/Mundial.java
@@ -9,6 +9,41 @@ package mundial;
  * @author USER
  */
 public class Mundial {
-
     
+private String nombre;
+private int año;
+private String Pais;
+private String Selecciones;
+private String Partidos;
+
+
+//metodo get
+public String getNombre(){
+    return this.nombre;
 }
+public int getAño(){
+    return this.año;
+}
+public String getPais(){
+    return this.Pais;
+}
+public String getSelecciones(){
+    return this.Selecciones;
+}
+public String getPartidos(){
+    return this.Partidos;
+}
+public Mundial(String nombre,int año,String Pais,String Selecciones,String Partidos){
+    this.nombre=nombre;
+    this.año=año;
+    this.Pais=Pais;
+    this.Selecciones=Selecciones;
+    this.Partidos=Partidos;
+}
+public String toString(){
+    return "nombre"+ this.nombre +"\nAño"+ this.año+ "\nPais"+ this.Pais+ "\nSelecciones"+ this.Selecciones+ "\nPartidos"+ Partidos;
+}
+
+}
+
+
