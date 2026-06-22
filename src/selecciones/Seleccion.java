dde37bb9721c39a9ce610758bebc7d998c0da7c8
EMMAXZZ <emmnuelpinda@gmail.com>
2026-06-22 15:18:18 -0600

    clase seleccion
    se corrigio la clase y se añadieron 5 metodos agregar,eliminar,buscarnombre,buscarcamisa,contarjugadorea, existecamisa

--- src/selecciones/Seleccion.java
+++ src/selecciones/Seleccion.java
@@ -8,12 +8,14 @@ import jugadores.Jugador;
 
 
 
-public class Seleccion extends Jugador {
+
+public class Seleccion  {
 
     private String nombre;
     private String grupo;
     private String entrenador;
-    private String[] jugadores;
+    private Jugador[] jugadores;
+    private int cantidadJ;
 
     public String getNombre() {
         return nombre;
@@ -35,20 +37,98 @@ public class Seleccion extends Jugador {
         this.entrenador = entrenador;
     }
 
-    public String[] getJugadores() {
+    public Jugador[] getJugadores() {
         return jugadores;
     }
     
      public Seleccion(String nombre, String grupo) {
-        this.nombre = nombre;
+       this.nombre = nombre;
         this.grupo = grupo;
-        this.jugadores = new String[23];
+        this.jugadores = new Jugador[23];
+        this.cantidadJ = 0;
         
     }
-     public void agregarJugador(jugador jugador) {
+     public void agregarJugador(Jugador jugador) {
+    if (cantidadJ >= jugadores.length) {
+            System.out.println("La selección ya tiene 23 jugadores.");
+            return;}
+    if (existeCamisa(jugador.getNumero())) {
+            System.out.println("Ya existe un jugador con la camiseta #" + jugador.getNumero());
+            return;} 
+     jugadores[cantidadJ] = jugador;
+        cantidadJ++;
+        System.out.println(jugador.getNombre() + " agregado correctamente.");
+        
+        
+    }
+    public void eliminarJugador(Jugador jugador) {
+        for (int i = 0; i < cantidadJ; i++) {
+            if (jugadores[i].equals(jugador)) {
+                // Desplaza los elementos hacia la izquierda
+                for (int j = i; j < cantidadJ - 1; j++) {
+                    jugadores[j] = jugadores[j + 1];
+                }
+                jugadores[cantidadJ - 1] = null;
+                cantidadJ--;
+                System.out.println(jugador.getNombre() + " eliminado correctamente.");
+                return;
+            }
+        }
+        System.out.println("Jugador no encontrado.");
+    }
     
+     public Jugador buscarPorNombre(String nombre) {
+        for (int i = 0; i < cantidadJ; i++) {
+            if (jugadores[i].getNombre().equalsIgnoreCase(nombre)) {
+                return jugadores[i];
+            }
+        }
+        System.out.println("No se encontró ningún jugador con el nombre: " + nombre);
+        return null;
+    }
+     
+      public Jugador buscarPorCamiseta(int numero) {
+        for (int i = 0; i < cantidadJ; i++) {
+            if (jugadores[i].getNumero() == numero) {
+                return jugadores[i];
+            }
+        }
+        System.out.println("No se encontró ningún jugador con la camiseta #" + numero);
+        return null;
+    }
+
+      public int contarJugadores() {
+        return cantidadJ;
+    }
+      
+        public boolean existeCamisa(int numero) {
+        for (int i = 0; i < cantidadJ; i++) {
+            if (jugadores[i].getNumero() == numero) {
+                return true;
+            }
+        }
+        return false;
+    }
+        
+        @Override
+public String toString() {
+    String resultado = "Selección: " + nombre +
+                       "\nGrupo: " + grupo +
+                       "\nEntrenador: " + entrenador +
+                       "\nJugadores registrados: " + cantidadJ +
+                       "\n---------------------------------\n";
+    
+    for (int i = 0; i < cantidadJ; i++) {
+        resultado += (i + 1) + ". " + jugadores[i].getNombre() + 
+                     " | Camiseta #" + jugadores[i].getNumero() + 
+                     " | Posicion: " + jugadores[i].getPosicion() + "\n";
+    }
+    
+    return resultado;
+}
 }
 
-public void eliminarJugador(jugador jugador) {
-    
-}
+
+
+
+
