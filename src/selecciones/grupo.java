aa38ca8f8da1873a68f2f127cce6cf402a53ed4c
Daniel <danielariassolano@gmail.com>
2026-06-22 16:57:16 -0600

    se arreglo mundial

--- src/selecciones/grupo.java
+++ src/selecciones/grupo.java
@@ -9,5 +9,9 @@ package selecciones;
  * @author EMMAXZZ
  */
 public enum grupo {
-    A, B, C, D, E, F, G, H,I,J,K,L,M,N,O,P,Q,R,S,T,W,X,Y,Z
+    A, B, C, D, E, F, G, H,I,J,K,L,M,N,O,P,Q,R,S,T,W,X,Y,Z;
+
+    public boolean equalsIgnoreCase(String grupo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
